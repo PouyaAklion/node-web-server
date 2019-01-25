8805946182354bc13177d7cddc2e29d0fed57f5a
Pouya Aklion <pouyaakn4@gmail.com>
2019-01-26 00:45:44 +0330

    create project

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = new express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	let now = new Date().toString();
+	let log = `${now}: ${req.method} ${req.path} \n`
+	fs.appendFile('server.log', log, (err) => {
+		if (err) {
+			console.log("Unable to ");
+		}
+	})
+	next();
+})
+
+app.use((req, res, next) => {
+		res.render('maintenance.hbs');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'welcome to home page',
+	});
+})
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
+})
+
+app.listen("3000", () => {
+	console.log("Server is up on port 3000");
+});
