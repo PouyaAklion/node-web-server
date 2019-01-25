a9c07c41eab4ab8aafc463334db04979e15a7a73
Pouya Aklion <pouyaakn4@gmail.com>
2019-01-26 01:22:57 +0330

    create bot.js

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Telegraf = require('telegraf')
+
+const bot = new Telegraf('AAE8JYePRfnNV9jEB1d7sPB4Vy9ktIxwzFo')
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
